a0366eecd84e10c77914985c5ae67a7b657158e4
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-03-07 09:52:12 -0400

    Align self example

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -16,19 +16,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#28C4D9',
     flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
   },
   box1: {
     borderWidth: 2,
     borderColor: 'white',
+    alignSelf: 'center',
   },
   box2: {
     borderWidth: 2,
     borderColor: 'white',
+    alignSelf: 'flex-start',
   },
   box3: {
     borderWidth: 2,
     borderColor: 'white',
+    alignSelf: 'flex-end',
   },
 });
